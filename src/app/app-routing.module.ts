b8e82102465c3b0fd1d1274b5698be65d445bdd8
Andony__7 <githubdaniel4@gmail.com>
2023-08-17 14:24:08 -0600

    [Init] Se inicio el proyecto (portafolio Web)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './component/content/content.component';
+import { ContactComponent } from './component/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "", component:ContentComponent
+  },
+  {
+    path: "contacto", component:ContactComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {StyleClassModule} from 'primeng/styleclass';
+
+
+
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import { ContentComponent } from './component/content/content.component';
+import { ContactComponent } from './component/contact/contact.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    StyleClassModule
   ],
   providers: [],
   bootstrap: [AppComponent]
